246882f9f168f7002f5794c22e2b3e202736533c
Riukotashi <kevin.monnot@protonmail.com>
2023-07-05 12:08:09 +0200

    feat: adding instances

--- terraform/modules/ops-instances/main.tf
+++ terraform/modules/ops-instances/main.tf
@@ -3,3 +3,19 @@
 resource "openstack_compute_instance_v2" "test-server" {
   name = "toto"
 }
+
+resource "openstack_compute_instance_v2" "basic" {
+  name            = "basic"
+  image_id        = "ad091b52-742f-469e-8f3c-fd81cadf0743"
+  flavor_id       = "3"
+  key_pair        = "my_key_pair_name"
+  security_groups = ["default"]
+
+  metadata = {
+    this = "that"
+  }
+
+  network {
+    name = "my_network"
+  }
+}
